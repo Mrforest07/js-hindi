93c97c12b1ae5412bad72b0b8ff07e4528a42efc
Aranya Das <161953395+Mrforest07@users.noreply.github.com>
2026-09-04 06:10:09 +0000

    data types

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,21 @@
+const accountId= 144553
+let accountEmail="abc@google.com"
+var accountPassword="12345"
+accounCity="Kolkata"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail="wwewiw"
+accountPassword="857575"
+
+
+console.log(accountId);
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+
+*/
+
+console.table([accountEmail,accountId,accountPassword,accounCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";// treat all js code as newer version
+
+//alert(3+3) //we are using node js not browser
+
+let name="aranya"
+let age= 18
+let isLoggedIn=false
+
+
+// number
+// bigint
+// string=>""
+// boolean=> true/false
+// null=> standalone value
+// undefined=> variable declared but value is not assigned
+// symbol =>unique
+
+//object
+
+console.log(typeof "name");
+
