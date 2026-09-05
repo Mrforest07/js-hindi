0346223d8c87545443740f2994caa53767e8e3da
Aranya Das <161953395+Mrforest07@users.noreply.github.com>
2026-09-05 13:35:29 +0000

    dataOpeartions

---
+++ 01_basics/03_dataTypeConversion.js
@@ -0,0 +1,23 @@
+//dataTypeConversion
+let score = null
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn = "a"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 => true; 
+//""=> false;
+//"Aranya"=> true;
+
+let someNumbr = 33
+
+let stringNumber = String(someNumbr);
+console.log(stringNumber);
+console.log(typeof stringNumber); // string
---
+++ 01_basics/04_opeartions.js
@@ -0,0 +1,16 @@
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+let a = 3;
+const b= ++a;
+
+//console.log(`a:${a}, b:${b}`);
+
+console.log("2" > 1);
+console.log("02" >1)
+
+console.log(null>=0)
+console.log(null>0)
+console.log(null==0)
+console.log(null==undefined)
