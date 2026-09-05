294396986a3d24f87c9b484201c54bd236f5e80e
Aranya Das <161953395+Mrforest07@users.noreply.github.com>
2026-09-05 14:31:39 +0000

    stackandHeap

---
+++ 01_basics/memories.js
@@ -0,0 +1,18 @@
+let myName="Aranya";
+
+let anotherName=myName;
+anotherName="Forest";
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne={
+    email:"aranya@example.com"
+};
+
+let userTwo=userOne;
+
+userTwo.email="forest@example.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
